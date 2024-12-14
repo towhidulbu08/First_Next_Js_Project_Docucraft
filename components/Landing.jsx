c0a9987fa783684fc2bef5e023df76b223a79f1e
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-12-14 18:53:05 +0600

    error solved

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -13,9 +13,9 @@ const Landing = () => {
               </h1>
 
               <p className="mt-6 text-base leading-7 text-slate-700">
-                It doesn't matter if you have an API if nobody knows how to use
-                it. Teach people the ins and outs of OAuth 2.0 and JWTs in style
-                with Protocol, a beautiful API documentation template.
+                It doesn&apos;t matter if you have an API if nobody knows how to
+                use it. Teach people the ins and outs of OAuth 2.0 and JWTs in
+                style with Protocol, a beautiful API documentation template.
               </p>
 
               <div className="mt-10 flex flex-wrap gap-4">
--- components/SearchResult.jsx
+++ components/SearchResult.jsx
@@ -5,7 +5,7 @@ export default function SearchResult({ results, term, closeSearchResults }) {
     <div className="absolute left-0 top-12 z-[999] w-full rounded-md bg-white p-4 shadow">
       <p class="!text-lg">
         Showing results for
-        <span class="font-semibold"> "{term}"</span>
+        <span class="font-semibold"> &quot;{term}&quot;</span>
       </p>{" "}
       <ul role="list" class="divide-y divide-gray-100 [&>*]:py-2">
         {results.map((result) => (
